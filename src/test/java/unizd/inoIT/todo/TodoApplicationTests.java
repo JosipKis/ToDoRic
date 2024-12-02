629d6adb5516cf22c447b0eebd48a41b0d5c3779
JosipKis <josip.kis2@gmail.com>
2024-12-02 14:07:01 +0100

    Initial commit

---
+++ src/test/java/unizd/inoIT/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package unizd.inoIT.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
